154b1646abac1cfe9c853b5a58abd6cbef50102d
Shreyas B1 <shreyas.b1@olacabs.com>
2019-10-27 00:36:14 +0530

    DataStreams

--- src/main/java/dev/shreyas/flink/course/batching/joins/main/BatchOtherJoinsExample.java
+++ src/main/java/dev/shreyas/flink/course/batching/joins/main/BatchOtherJoinsExample.java
@@ -3,6 +3,7 @@ package dev.shreyas.flink.course.batching.joins.main;
 import dev.shreyas.flink.course.utils.InputUtil;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.common.functions.RichJoinFunction;
+import org.apache.flink.api.common.operators.base.JoinOperatorBase;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -26,8 +27,9 @@ public class BatchOtherJoinsExample {
     public static class Join extends RichJoinFunction<Tuple2<Integer,String>,Tuple2<Integer,String>,Tuple3<Integer,String,String>> {
         @Override
         public Tuple3<Integer, String, String> join(Tuple2<Integer, String> personDataSet, Tuple2<Integer, String> locationDataSet) throws Exception {
-            // Since it's left outer join, location can be Null
+            // Since it's left outer join, location can be null
             // In case of right outer join, person can be null
+            // In case of full outer join, both person and location can be null
             if (locationDataSet == null){
                 return new Tuple3<>(personDataSet.f0,personDataSet.f1,"NULL");
             }
@@ -48,7 +50,7 @@ public class BatchOtherJoinsExample {
                 .flatMap(new BatchInnerJoinExample.ReadInput());
 
         // perform left outer join
-        DataSet<Tuple3<Integer,String,String>> leftOuterJoin = personDataSet.leftOuterJoin(locationDataSet).where(0).equalTo(0)
+        DataSet<Tuple3<Integer,String,String>> leftOuterJoin = personDataSet.leftOuterJoin(locationDataSet, JoinOperatorBase.JoinHint.BROADCAST_HASH_FIRST).where(0).equalTo(0)
                 .with(new Join());
         leftOuterJoin.print();
         /*
@@ -60,4 +62,28 @@ public class BatchOtherJoinsExample {
             (5,Robert,NULL)
          */
     }
+
+
+    /*
+        JoinHint (source:https://ci.apache.org/projects/flink/flink-docs-stable/dev/batch/dataset_transformations.html)
+        The following hints are available:
+
+        OPTIMIZER_CHOOSES: Equivalent to not giving a hint at all, leaves the choice to the system.
+
+        BROADCAST_HASH_FIRST: Broadcasts the first input and builds a hash table from it, which is probed (examined/checked) by the second input.
+         A good strategy if the first input is very small.
+
+        BROADCAST_HASH_SECOND: Broadcasts the second input and builds a hash table from it, which is probed by the first input.
+        A good strategy if the second input is very small.
+
+        REPARTITION_HASH_FIRST: The system partitions (shuffles) each input (unless the input is already partitioned) and builds a hash table from the first input.
+        This strategy is good if the first input is smaller than the second, but both inputs are still large.
+        Note: This is the default fallback strategy that the system uses if no size estimates can be made and no pre-existing partitions and sort-orders can be re-used.
+
+        REPARTITION_HASH_SECOND: The system partitions (shuffles) each input (unless the input is already partitioned) and builds a hash table from the second input.
+        This strategy is good if the second input is smaller than the first, but both inputs are still large.
+
+        REPARTITION_SORT_MERGE: The system partitions (shuffles) each input (unless the input is already partitioned) and sorts each input (unless it is already sorted).
+        The inputs are joined by a streamed merge of the sorted inputs. This strategy is good if one or both of the inputs are already sorted.
+     */
 }
---
+++ src/main/java/dev/shreyas/flink/course/datastreams/basics/main/WordCountSocketExample.java
@@ -0,0 +1,56 @@
+package dev.shreyas.flink.course.datastreams.basics.main;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+// Basic Socket stream Word count Example
+
+/* To Run
+
+ Open Terminal 1: nc -l 9999
+ Open Terminal 2: flink run -c dev.shreyas.flink.course.datastreams.basics.main.WordCountSocketExample path/to/jar/file
+
+
+Test Output:
+    (shreyas,1)
+    (shreyas,2)
+    (shreyas,3)
+    (wassup,1)
+    (people,1)
+    (shreyaas,1)
+ */
+
+public class WordCountSocketExample {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // Listens to socket port 9999 and reads the input
+        DataStream<String> text = env.socketTextStream("localhost",9999);
+        DataStream<Tuple2<String,Integer>> counts =
+                text.flatMap(new Tokenizer())
+                .keyBy(0) // Equivalent to Group By field
+                .sum(1); // initial sum
+        counts.print();
+        env.execute("Word Count example");
+    }
+
+    // User-defined functions
+    public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+}
